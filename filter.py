7c22bdcddd0515564f01adbeb4947c9acddd92b6
btwl <1253102853@qq.com>
2024-08-23 19:46:48 +0800

    first commit

---
+++ filter.py
@@ -0,0 +1,13 @@
+# Open the file in read mode
+with open('word_list_4000.txt', 'r') as file:
+    lines = file.readlines()
+
+# Open a new file in write mode to save the filtered words
+with open('filtered_words.txt', 'w') as file:
+    for line in lines:
+        # Check if the line contains a colon
+        if ':' in line:
+            # Split the line at the colon and take the first part (the word)
+            word = line.split(':')[0].strip()
+            # Write the word to the new file
+            file.write(word + '\n')
